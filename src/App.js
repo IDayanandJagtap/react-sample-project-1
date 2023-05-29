822aa55be8fa960f9641b69426f8837df0bd52f8
DJ <dayanandjagtap07@gmail.com>
2023-05-29 10:28:52 +0530

    Footer[footer.jsx] - Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./Components/Home.jsx"
 import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes >
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>TechyStar</h1>
+                <p>One place solutions to all problems.</p>
+            </div>
+            <div>
+                <h2>Follow Us</h2>
+                <div className="foot-links">
+                    <a href="https://instagram.com">Instagram</a>
+                    <a href="https://Facebook.com">Facebook</a>
+                    <a href="https://twitter.com">Twitter</a>
+                    <a href="https://Youtube.com">Youtube</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
