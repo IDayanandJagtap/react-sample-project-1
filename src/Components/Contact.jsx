4d8ac19c71dec3791b4601f55c0db55989315236
DJ <dayanandjagtap07@gmail.com>
2023-06-01 03:58:34 +0530

    Design - make site responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,22 +2,31 @@ import React from 'react'
 
 
 const Contact = () => {
+
+  const handleOnSubmit = (e) => { 
+    e.preventDefault();
+    alert("Form submitted !")
+    document.getElementsByName("name")[0].value = ""
+    document.getElementsByName("email")[0].value = ""
+    document.getElementsByName("message")[0].value = ""
+  }
+
   return (
     <div className='contact' id='contact'>
       <main>
         <h1>Contact us</h1>
-        <form className='contactform'>
+        <form className='contactform' onSubmit={handleOnSubmit}>
             <div>
                 <label>Name</label>
-                <input type="text" required placeholder='Abc'/>
+                <input type="text" required placeholder='Abc' name='name'/>
             </div>
             <div>
                 <label>Email</label>
-                <input type="email" required placeholder='abc@xyz.com'/>
+                <input type="email" required placeholder='abc@xyz.com' name="email"/>
             </div>
             <div>
                 <label>Message</label>
-                <input type="text" required placeholder='Tell us your queries ... ' />
+                <input type="text" required placeholder='Tell us your queries ... ' name='message'/>
             </div>
             <div id='btnContainer'>
                 <button type='submit'>Submit</button>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {HashLink} from "react-router-hash-link"
+import {AiOutlineCloseCircle} from "react-icons/ai"
+import {RxHamburgerMenu} from "react-icons/rx"
 
 
 const Header = () => {
+
+  const hamDiv = useRef();
+
+  const toggleHamMenu = () =>{
+    if(hamDiv.current.style.top === "-110vh")
+      hamDiv.current.style.top = "0";
+    else 
+      hamDiv.current.style.top = "-110vh";
+      
+  }
+
+
+
+
   return (
     <nav className='nav'>
         <div className="nav-heading">
@@ -13,6 +29,20 @@ const Header = () => {
           <HashLink to="/#about" >About</HashLink>
           <HashLink to="/#brands" >Brands</HashLink>
           <HashLink to="/contact/#contact" >Contact</HashLink>
+        </div>
+        <div className="hamMenuBtn">
+          <RxHamburgerMenu onClick={toggleHamMenu}/>
+        </div>
+        <div className="hamMenu" ref={hamDiv}>
+        <div className="close-ham">
+          <AiOutlineCloseCircle onClick={toggleHamMenu}/>
+        </div>
+          <div className="ham-nav-links">
+            <HashLink  onClick={toggleHamMenu} to="/#home" >Home</HashLink>
+            <HashLink  onClick={toggleHamMenu} to="/#about" >About</HashLink>
+            <HashLink  onClick={toggleHamMenu} to="/#brands" >Brands</HashLink>
+            <HashLink  onClick={toggleHamMenu} to="/contact/#contact" >Contact</HashLink>
+          </div>
         </div>
     </nav>
   )
